53b22fbddb3b6545c8101b281474ab6e62af913d
Aayush singh <saayush615@gmail.com>
2025-11-01 15:57:47 +0530

    refactor(React-Router): setup react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '@/pages/Home'
+import About from '@/pages/About'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/about' element={ <About /> } />
+      </Routes>
     </>
   )
 }
