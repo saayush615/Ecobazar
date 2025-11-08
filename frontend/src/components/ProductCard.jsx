c600c29ad7fc650da7e70d260c83d8ea19079b1a
Aayush singh <saayush615@gmail.com>
2025-11-08 09:01:23 +0530

    feat(Product Card): add Product Card UI

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { ShoppingBag } from 'lucide-react';
+
 import {
   Card,
   CardAction,
@@ -10,34 +12,37 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-const ProductCard = ({ title, source, category }) => {
+const ProductCard = ({ name, source, originalPrice, discountedPrice}) => {
   return (
-        <Card className={`cursor-pointer transition-all duration-300 ease-in-out hover:border-green-500 hover:shadow-[0_0_20px_rgba(74,222,128,0.4)] hover:scale-[1.02] hover:text-green-500`}>
+    <div>
+        <Card className={`cursor-pointer transition-all duration-300 ease-in-out hover:border-green-700 group`}>
 
-            <CardHeader className='p-2 sm:p-3 md:p-4 space-y-1'>
+            <CardHeader>
                 <img src={source} alt="Category" className='w-full h-20 sm:h-24 md:h-28 object-contain' />
-                <CardTitle className='text-xs sm:text-sm md:text-base text-center truncate pt-1'>{title}</CardTitle>
-                {!category && (
-                    <>
-                        <CardDescription></CardDescription>
-                        <CardAction></CardAction>
-                    </>
-                )}
             </CardHeader>
 
-            {!category && (
-                <>
-                    <CardContent>
-                        <p></p>
-                    </CardContent>
+            <CardContent className={`px-2`}>
+                <p className='text-sm transition-colors duration-300 group-hover:text-green-700'>{name}</p>
 
-                    <CardFooter>
-                        <p></p>
-                    </CardFooter>
-                </>
-            )}
+                <div className='flex flex-row justify-between items-center'>
+                    <div>
+                        <span className='font-semibold text-gray-900'>{discountedPrice}</span>
+                        {originalPrice !== discountedPrice && (
+                            <span className='text-xs text-gray-500 line-through p-2'>{originalPrice}</span>
+                        )}
+                    </div>
+
+                    <ShoppingBag className='size-9 p-2 hover:bg-green-500 rounded-2xl' />
+                </div>
+
+            </CardContent>
+
+            <CardFooter>
+                <p className='text-xs p-1'>⭐⭐⭐⭐⭐</p>
+            </CardFooter>
 
         </Card>
+    </div>
   )
 }
 
