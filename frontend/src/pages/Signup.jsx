486573b48d2ec18dcb5a5394fcd97ff73669127d
Aayush singh <saayush615@gmail.com>
2025-11-14 16:01:22 +0530

    feat(ui): Add header and footer to login & signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const Signup = () => {
   return (
     <div>
-      
+      <Header />
+        <p>Hello</p>
+      <Footer />
     </div>
   )
 }
