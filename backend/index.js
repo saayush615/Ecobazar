2576d094ff94d71c1a476225b35b0202fa7c3edf
Aayush singh <saayush615@gmail.com>
2025-11-30 13:43:26 +0530

    chore: commented admin route temporarily

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,13 @@ import './config/passport.js'
 import { connectToDB } from './config/database.js'
 
 import { checkAuthentication } from './middlewares/auth.js';
-import { adminOnly } from './middlewares/admin.js';
+// import { adminOnly } from './middlewares/admin.js';
 import globalErrorHandler from './middlewares/errorHandler.js'
 
 import userRoute from './routes/user.js';
 import productRoute from './routes/product.js';
 import cartRoute from './routes/cart.js';
-import adminRoute from './routes/admin.js';
+// import adminRoute from './routes/admin.js';
 import orderRoute from './routes/order.js';
 import oAuthRoute from './routes/oauth.js';
 
@@ -56,7 +56,7 @@ app.use('/user', userRoute);
 app.use('/product', productRoute);
 app.use('/cart', cartRoute);
 app.use('/order', orderRoute);
-app.use('/admin', adminOnly, adminRoute);
+// app.use('/admin', adminOnly, adminRoute);
 app.use('/oauth', oAuthRoute);
 
 app.use((req,res) => {
