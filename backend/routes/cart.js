d167724550dfe177e90d4fa7aecc0ea4cf314c21
Aayush singh <saayush615@gmail.com>
2025-12-24 22:39:12 +0530

    feat(backend): add getCartItem route

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { handleAddToCart,handleProdRemove } from '../controllers/cart.js';
+import { handleAddToCart,handleProdRemove, handleGetCartItems } from '../controllers/cart.js';
 
 const router = express.Router();
 
 router.post('/remove/:id', handleProdRemove);
 router.post('/:id', handleAddToCart);
+router.get('/', handleGetCartItems)
 
 export default router;
