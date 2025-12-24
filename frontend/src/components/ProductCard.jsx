07e54184ae1992caac73f9cd440fb516b4ebd9c5
Aayush singh <saayush615@gmail.com>
2025-12-24 11:29:34 +0530

    feat: replace frontend dummy product data with backend integration

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -19,7 +19,7 @@ const ProductCard = ({ name, source, originalPrice, discountedPrice}) => {
         <Card className={`cursor-pointer transition-all duration-300 ease-in-out hover:border-green-700 group`}>
 
             <CardHeader className='relative'>
-                <img src={source} alt="Category" className='w-full h-20 sm:h-24 md:h-28 object-contain' />
+                <img src={`${import.meta.env.VITE_API_URL}${source}`} alt="Category" className='w-full h-20 sm:h-24 md:h-28 object-contain' />
 
                 {/* Favourite */}
                 <div className='absolute top-2 right-2'>
