b53430a1c8cd2ac259ddbbd8acaf1f73b057cc18
Aayush singh <saayush615@gmail.com>
2026-01-04 20:50:58 +0545

    feat: add additional fields for payment

--- backend/models/order.js
+++ backend/models/order.js
@@ -17,6 +17,31 @@ const orderSchema = mongoose.Schema({
             required: true
         }
     }],
+
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['razorpay', 'cod'],
+        default: 'cod'
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending'
+    },
+    razorpayOrderId: {
+        type: String
+    },
+    razorpayPaymentId: {
+        type: String
+    },
+    razorpaySignature: {
+        type: String
+    },
+    
     status: {
         type: String,
         required: true,
