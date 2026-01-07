23446c5cf82477cc1a0fac35cfff3d5daa6a01e2
Aayush singh <saayush615@gmail.com>
2026-01-07 23:38:30 +0545

    feat: add all order routes

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { handleBuyNow } from '../controllers/order.js';
+import { handleCreateOrder, handleVerifyPayment, handlePaymentFailure } from '../controllers/order.js';
 
 const router = express.Router();
 
-router.post('/BuyNow', handleBuyNow);
+router.post('/create-order', handleCreateOrder);
+router.post('/verify-payment', handleVerifyPayment);
+router.post('/payment-failure', handlePaymentFailure);
 
 export default router;
