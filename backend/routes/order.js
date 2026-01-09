74128e1845abde0f24d30e4596f0ff689a170b7c
Aayush singh <saayush615@gmail.com>
2026-01-09 12:12:24 +0530

    feat: add cod route

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { handleCreateOrder, handleVerifyPayment, handlePaymentFailure } from '../controllers/order.js';
+import { handleCreateOrder, handleVerifyPayment, handlePaymentFailure, handleCODOrder } from '../controllers/order.js';
 
 const router = express.Router();
 
 router.post('/create-order', handleCreateOrder);
 router.post('/verify-payment', handleVerifyPayment);
 router.post('/payment-failure', handlePaymentFailure);
+
+router.post('/cod-order', handleCODOrder);
 
 export default router;
