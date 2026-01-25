e9902eadb9210b084cadb8920c3990b5409e026b
Aayush singh <saayush615@gmail.com>
2026-01-25 16:12:46 +0530

    feat: add Map section with google map embedding

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -226,8 +226,17 @@ const Contact = () => {
             </div>
 
             {/* Map Section */}
-            <div>
-
+            <div className='rounded-xl shadow-md overflow-hidden border border-gray-100 dark:border-gray-700'>
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15121.420715184951!2d73.75955285000002!3d18.64805110000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9ef5800a8e3%3A0xf67c2a02f5febe56!2sAkurdi!5e0!3m2!1sen!2sin!4v1769336859519!5m2!1sen!2sin" 
+                width="100%" 
+                height="100%" 
+                style= {{ border:0 }}
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade"
+              >
+              </iframe>
             </div>
           </div>
         </div>
