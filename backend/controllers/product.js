b756f20a0563b5ee8f3aeb08c356358ba0f2ad5b
Aayush singh <saayush615@gmail.com>
2026-01-26 13:58:38 +0530

    feat: add handler for find product by id

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,4 +1,5 @@
 import Product from '../models/product.js';
+import { createNotFoundError, createValidationError } from '../utils/ErrorFactory.js';
 
 async function handleGetAllProd(_req,res,next) {
     try {
@@ -20,6 +21,28 @@ async function handleGetAllProd(_req,res,next) {
     }
 }
 
+async function handleGetProdById(req,res,next) {
+    try {
+        const ProdId = req.params.id;
+        if (!ProdId) {
+            return next(createValidationError('ProductId is required'));
+        }
+
+        const product = await Product.findById(ProdId);
+        if (!product) {
+            return next(createNotFoundError('Product'));
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Product retrieved successfully',
+            product
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 async function handleGetProdByName(req,res,next) {
     try{
         const searchQuery = req.body.search;
@@ -37,4 +60,4 @@ async function handleGetProdByName(req,res,next) {
 }
 
 
-export { handleGetAllProd, handleGetProdByName };
+export { handleGetAllProd, handleGetProdById, handleGetProdByName };
