4cbd0eef92b5ce425412f30c2863121d2ba0a4e1
Aayush singh <saayush615@gmail.com>
2026-01-30 14:36:42 +0530

    feat: made wishlist page layout & add favCard to it

--- frontend/src/pages/Wishlist.jsx
+++ frontend/src/pages/Wishlist.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+
+import { useWishlist } from '@/hooks/useWishlist'
+import FavCard from '@/components/FavCard'
 
 const Wishlist = () => {
+    const { wishlistItems, removeFromWishlist } = useWishlist();
+
+    const handleRemoveFromWishlist = async (favoriteId) => {
+        const result = await removeFromWishlist(favoriteId);
+        
+        if (result.success) {
+        toast.success('Removed from Wishlist');
+        } else {
+        toast.error(result.error || 'Failed to remove');
+        }
+    }
+    
   return (
-    <div>
-      I am wishlist
+    <div className='flex flex-col justify-between gap-1 min-h-screen dark:bg-gray-900'>
+      <div>
+        <Header />
+        <div className='container mx-auto px-4'>
+            {wishlistItems.map((item) => (
+                <FavCard 
+                  key={item._id}
+                  id={item._id}
+                  productId={item.product?._id}
+                  name={item.product?.name}
+                  source={`${import.meta.env.VITE_API_URL}${item.product?.image}`}
+                  category={item.product?.category}
+                  discountPrice={item.product?.discountPrice}
+                  originalPrice={item.product?.originalPrice}
+                  stock={item.product?.stock}
+                  onRemove={handleRemoveFromWishlist}
+                />
+            ))
+            }
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
