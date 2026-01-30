a2c40f87c28d11159fe422c52450c36c75d99a17
Aayush singh <saayush615@gmail.com>
2026-01-30 14:38:07 +0530

    feat: made a basic cart page layout & cart card

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import CartCard from '@/components/CartCard';
+import { useCart } from '@/hooks/useCart';
 
 const Cart = () => {
+    const { cartData } = useCart();
+
   return (
-    <div>
-      I am cart
+    <div className='flex flex-col justify-between gap-1 min-h-screen dark:bg-gray-900'>
+      <div>
+        <Header />
+        <div className='container mx-auto px-4'>
+            {cartData.map((item) => 
+                (<CartCard key={item._id} 
+                    id={item._id}
+                    Pname={item.product?.name} 
+                    source={`${import.meta.env.VITE_API_URL}${item.product?.image}`} 
+                    category={item.product?.category} 
+                    stock={item.product?.stock} 
+                    discountPrice={item.product?.discountPrice} 
+                    originalPrice={item.product?.originalPrice}
+                    quantity={item.quantity}
+            />))}
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
