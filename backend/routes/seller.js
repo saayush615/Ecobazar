9b49f0b839d317a1a7d8f74c2599cef8f538fd41
Aayush singh <saayush615@gmail.com>
2026-02-08 14:45:32 +0530

    feat: add routes for seller orders and order history

--- backend/routes/seller.js
+++ backend/routes/seller.js
@@ -1,5 +1,13 @@
 import express from 'express';
-import { handlePostProd, handleUpdateProd, handleDeleteProd, handleShowAllProd } from '../controllers/seller.js';
+
+import { 
+    handlePostProd, 
+    handleUpdateProd, 
+    handleDeleteProd, 
+    handleShowAllProd, 
+    handleGetSellerOrders,
+    handleGetSellerOrderHistory  } from '../controllers/seller.js';
+
 import {upload } from '../config/upload.js'
 
 const router = express.Router();
@@ -8,5 +16,8 @@ router.get('/', handleShowAllProd);
 router.post('/', upload.single('image'), handlePostProd);
 router.put('/:id', upload.single('image'), handleUpdateProd);
 router.delete('/:id', handleDeleteProd);
+
+router.get('/orders', handleGetSellerOrders);
+router.get('/order-history', handleGetSellerOrderHistory);
 
 export default router;
