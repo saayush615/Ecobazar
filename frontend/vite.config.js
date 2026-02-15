e7e2ae5ae5a16c96705177a0cfb8ee9b4553451b
Aayush singh <saayush615@gmail.com>
2026-02-15 11:19:49 +0000

    chore: update frontend dependencies and configure image optimization
    - Added sharp and svgo for image processing in package.json.
    - Integrated vite-plugin-image-optimizer in vite.config.js with specified quality settings for png, jpeg, webp, and avif formats.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,20 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    ViteImageOptimizer({
+      png: { quality: 70 },
+      jpeg: { quality: 70 },
+      webp: { quality: 70 },
+      avif: { quality: 60 },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
