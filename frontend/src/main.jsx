1a8f0a37dfa44273a69f2f192fdb4a8008bf6d62
Aayush singh <saayush615@gmail.com>
2026-07-30 17:26:55 +0530

    feat: setup tanstack query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,17 +4,26 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from './contexts/ThemeContext'
 import { Provider } from 'react-redux'
 import { store } from './store/store';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from './lib/queryClient';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </Provider>
+
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+      
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
