6e685698feb74968699618e9cc4b027d81865a5e
Aayush singh <saayush615@gmail.com>
2026-08-03 12:43:40 +0530

    refactor: product controller with asyncHandler

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,78 +1,65 @@
-import product from '../models/product.js';
 import Product from '../models/product.js';
+import asyncHandler from '../utils/asyncHandler.js';
 import { createNotFoundError, createValidationError } from '../utils/ErrorFactory.js';
 
-async function handleGetAllProd(_req,res,next) {
-    try {
-        const products = await Product.find();
-        if (products.length === 0) {
-            return res.status(200).json({
-                success: true,
-                message: 'No Product Added',
-                products: []
-            })
-        }
+const handleGetAllProd = asyncHandler(async (_req,res,next) => {
+    const products = await Product.find();
+    if (products.length === 0) {
         return res.status(200).json({
             success: true,
-            message: 'Product reterived successfully',
-            products
+            message: 'No Product Added',
+            products: []
         })
-    } catch (error) {
-        next(error);
     }
-}
+    return res.status(200).json({
+        success: true,
+        message: 'Product reterived successfully',
+        products
+    })
+})
 
-async function handleGetProdById(req,res,next) {
-    try {
-        const ProdId = req.params.id;
-        if (!ProdId) {
-            return next(createValidationError('ProductId is required'));
-        }
+const handleGetProdById = asyncHandler(async (req,res,next) => {
+    const ProdId = req.params.id;
+    if (!ProdId) {
+        return next(createValidationError('ProductId is required'));
+    }
 
-        const product = await Product.findById(ProdId);
-        if (!product) {
-            return next(createNotFoundError('Product'));
-        }
+    const product = await Product.findById(ProdId);
+    if (!product) {
+        return next(createNotFoundError('Product'));
+    }
 
+    return res.status(200).json({
+        success: true,
+        message: 'Product retrieved successfully',
+        product
+    })
+})
+
+const handleGetFilteredByCategoryData = asyncHandler(async (req,res,next) => {
+    const category = req.params.category;
+    if (!category) {
+        return next(createValidationError('category is required'));
+    };
+
+    const normalizedCategory = category.replace(/-and-/g, ' & ')
+            .replace(/-/g, ' ')
+            .replace(/\b\w/g, (char) => char.toUpperCase());
+
+    const filteredData = await Product.find({ category: normalizedCategory});
+    if (filteredData.length === 0) {
         return res.status(200).json({
             success: true,
-            message: 'Product retrieved successfully',
-            product
+            message: 'No Product of this category',
+            products: []
         })
-    } catch (error) {
-        next(error)
     }
-}
 
-async function handleGetFilteredByCategoryData(req,res,next) {
-    try {
-        const category = req.params.category;
-        if (!category) {
-            return next(createValidationError('category is required'));
-        };
-
-        const normalizedCategory = category.replace(/-and-/g, ' & ')
-                .replace(/-/g, ' ')
-                .replace(/\b\w/g, (char) => char.toUpperCase());
-
-        const filteredData = await Product.find({ category: normalizedCategory});
-        if (filteredData.length === 0) {
-            return res.status(200).json({
-                success: true,
-                message: 'No Product of this category',
-                products: []
-            })
-        }
-
-        return res.status(200).json({
-            success: true,
-            message: 'Product get successfully',
-            products: filteredData
-        })
-    } catch (error) {
-        next(error);
-    }
-}
-
+    return res.status(200).json({
+        success: true,
+        message: 'Product get successfully',
+        products: filteredData
+    })
+})
 
 export { handleGetAllProd, handleGetProdById, handleGetFilteredByCategoryData };
