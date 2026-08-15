2f9733eff2c28d0a2b0a34f576dc231ec58ff4c2
Aayush singh <saayush615@gmail.com>
2026-08-15 17:05:16 +0530

    feat: add query validation middleware for product search route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { handleGetAllProd, handleGetProdById, handleGetFilteredByCategoryData, handleSearchProducts } from '../controllers/product.js';
+import { validateQuery } from '../middlewares/validate.js';
 
 const router = express.Router();
 
 router.get('/all', handleGetAllProd);
-router.get('/search', handleSearchProducts);
+router.get('/search', validateQuery(searchProductQuerySchema), handleSearchProducts);
 router.get('/:id', handleGetProdById);
 router.get('/filter/:category', handleGetFilteredByCategoryData);
 
